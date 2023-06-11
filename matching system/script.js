a98dd0ad02b3544ff233bd3c0e54b05b8a677f04
yx-05 <72589058+yx-05@users.noreply.github.com>
2023-06-12 00:31:36 +0800

    test file uploaded
    test file for the matching system

---
+++ matching system/script.js
@@ -0,0 +1,6 @@
+var percentage = document.getElementsByClassName("percentage")
+var value = document.getElementById("mbti")
+var button = document.getElementsByClassName("button")
+function log(){
+    console.log(value.value)
+}
