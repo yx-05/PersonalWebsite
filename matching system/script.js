357a251504387ecd418d975495d28c6bbf78e4d3
yx-05 <72589058+yx-05@users.noreply.github.com>
2023-06-12 20:59:57 +0800

    updated matching function

--- matching system/script.js
+++ matching system/script.js
@@ -1,6 +1,34 @@
-var percentage = document.getElementsByClassName("percentage")
-var value = document.getElementById("mbti")
-var button = document.getElementsByClassName("button")
-function log(){
-    console.log(value.value)
+var percentage = document.getElementById("percentage");
+var button = document.getElementsByClassName("button");
+
+const red = "red";
+const yellow = "yellow";
+const green = "green";
+const blue = "blue";
+
+const mbtis = {
+    estj: red,
+    esfj: red,  
+    istj: yellow,
+    isfj: red,
+    estp: yellow,
+    esfp: red,
+    istp: green,
+    isfp: yellow,
+    entj: green,
+    entp: blue,
+    intj: blue,
+    intp: blue, 
+    enfj: yellow,
+    enfp: green,
+    infj: green,
+    infp: blue,
+};
+
+function output(){
+    var mbti = String(document.getElementById("mbti").value);
+    console.log(mbti);
+    console.log(mbtis[mbti]);
+    
+    percentage.innerHTML = mbtis[mbti];
 }
